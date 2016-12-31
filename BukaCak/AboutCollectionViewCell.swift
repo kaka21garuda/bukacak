dac176f40f4644739c32bbacd7c6b67e5913af0f
Buka Cakrawala <kaka10garuda@gmail.com>
2016-12-31 15:13:42 -0800

    setup collection view in about and gradient

---
+++ BukaCak/AboutCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  AboutCollectionViewCell.swift
+//  BukaCak
+//
+//  Created by Buka Cakrawala on 12/31/16.
+//  Copyright © 2016 Buka Cakrawala. All rights reserved.
+//
+
+import UIKit
+
+class AboutCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var contentLabel: UILabel!
+    
+}
--- BukaCak/AboutViewController.swift
+++ BukaCak/AboutViewController.swift
@@ -8,7 +8,16 @@
 
 import UIKit
 
+struct About {
+    
+    let image: UIImage
+    let title: String
+    let text: String
+}
+
 class AboutViewController: UIViewController {
+    
+    let aboutLists: [About] = []
 
     @IBOutlet weak var dismissButton: UIButton!
     
@@ -16,9 +25,12 @@ class AboutViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //colorGradientBackground()
         dismissButton.layer.cornerRadius = dismissButton.frame.size.width / 2
     }
 
@@ -26,5 +38,67 @@ class AboutViewController: UIViewController {
         super.didReceiveMemoryWarning()
         
     }
+    
+    func colorGradientBackground() {
+    
+        let topColor = UIColor(red: 233, green: 100, blue: 67, alpha: 1)
+        let bottomColor = UIColor(red: 144, green: 78, blue: 149, alpha: 1)
+        
+        let gradientColor: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
+        let gradientLocations: [Float] = [0.0, 1.0]
+        
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColor
+        gradientLayer.locations = gradientLocations as [NSNumber]?
+        
+        gradientLayer.frame = self.view.bounds
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+        
+    }
+}
+
+extension AboutViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "aboutCell", for: indexPath) as! AboutCollectionViewCell
+
+        cell.imageView.image = #imageLiteral(resourceName: "profile")
+        cell.titleLabel.text = "Hello world"
+        cell.contentLabel.text = "My name is Buka Cakrawala"
+        
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = ((view.frame.width) / 2) + 80
+        let height = ((view.frame.height) / 2 + 200)
+        
+        let cellSize = CGSize(width: width, height: height)
+        return cellSize
+        
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        
+//        let width = ((view.frame.width) - 40) * 3
+//        let cellSpacing = 10 * (2)
+//        
+//        let leftInset = (collectionView.frame.width - CGFloat(width) + CGFloat(cellSpacing)) / 2
+//        let rightInset = leftInset
+//        
+//        return UIEdgeInsetsMake(0, leftInset, 0, rightInset)
+//        
+//    }
     
 }
